1beb6052c585a8d9abbc85e3cd34bbcfd7dd79d6
Lindal <lindal@mail.ru>
2017-02-05 04:48:48 +0400

    1

--- src/main.cpp
+++ src/main.cpp
@@ -5,36 +5,454 @@
  */
 #include "Arduino.h"
 
-//Пин подключен к ST_CP входу 74HC595
-int latchPin = 8;
-//Пин подключен к SH_CP входу 74HC595
+//Пин подключен к ST_CP входу 74HC595 - 11
+int latchPin = 11;
+//Пин подключен к SH_CP входу 74HC595 - 12
 int clockPin = 12;
-//Пин подключен к DS входу 74HC595
-int dataPin = 11;
+//Пин подключен к DS входу 74HC595 - 10
+int dataPin = 10;
 
-
+int oePin = 15;
 
 void setup() {
- //устанавливаем режим OUTPUT
- pinMode(latchPin, OUTPUT);
- pinMode(clockPin, OUTPUT);
- pinMode(dataPin, OUTPUT);
+  //устанавливаем режим OUTPUT
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(oePin, OUTPUT);
+  analogWrite(oePin, 0);
+}
+
+unsigned int convertArrayToInt(unsigned int row[8]) {
+  unsigned int result = 0;
+  for (short i = 7; i >= 0; i--) {
+    if (row[i] == 1) {
+      result = (1 << i) | result;
+    }
+  }
+  return result;
+}
+
+void render(unsigned int snapshot[16][16]) {
+  unsigned int halfRow[2][8];
+  short index;
+  for (int i = 0; i < 16; i++) {
+    for (short n = 0; n < 2; n++) {
+      for (short x = 0; x < 8; x++) {
+        index = x + (n == 0 ? 0 : 8);
+        halfRow[n][x] = snapshot[i][index];
+      }
+    }
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, convertArrayToInt(halfRow[0]));
+    shiftOut(dataPin, clockPin, MSBFIRST, convertArrayToInt(halfRow[1]));
+    shiftOut(dataPin, clockPin, MSBFIRST, i >= 8 ? 1 << (i - 8) : 0);
+    shiftOut(dataPin, clockPin, MSBFIRST, i < 8 ? 1 << i : 0);
+    digitalWrite(latchPin, HIGH);
+  }
+}
+
+void renderByTime(unsigned int snapshot[16][16], int delay) {
+  unsigned long timer = millis();
+  while (millis() < timer + delay) {
+    render(snapshot);
+  }
+}
+
+void animation(unsigned long delay) {
+  unsigned long timer = millis();
+  unsigned int rowDelay = 100;
+  int stringCounter = 1;
+  int runningRow_1 = B01000000;
+  int runningRow_2 = B10000000;
+  int chess = B10101010;
+
+  while (millis() < timer + delay) {
+    for (int n = 0; n < 2; n++) {
+      int r = B00000001;
+      for (int i = 0; i < 8; i++) {
+        digitalWrite(latchPin, LOW);
+        if (n == 0) {
+          shiftOut(dataPin, clockPin, MSBFIRST, runningRow_1);
+          shiftOut(dataPin, clockPin, MSBFIRST, runningRow_1);
+          shiftOut(dataPin, clockPin, MSBFIRST, 0);
+          shiftOut(dataPin, clockPin, MSBFIRST, r);
+        } else {
+          shiftOut(dataPin, clockPin, MSBFIRST, chess);
+          shiftOut(dataPin, clockPin, MSBFIRST, chess);
+          shiftOut(dataPin, clockPin, MSBFIRST, r);
+          shiftOut(dataPin, clockPin, MSBFIRST, 0);
+        }
+        digitalWrite(latchPin, HIGH);
+        r = r << 1;
+      }
+    }
+
+    if (millis() > timer + rowDelay) {
+      stringCounter++;
+      runningRow_1 = runningRow_1 >> 1;
+      runningRow_2 = runningRow_2 >> 1;
+      chess = ~chess;
+      rowDelay += 100;
+    }
+    if (stringCounter > 8) {
+      stringCounter = 0;
+      runningRow_1 = B01000000;
+      runningRow_2 = B10000000;
+    }
+  }
+}
+
+void fillFull() {
+  for (int n = 0; n < 2; n++) {
+    int r = B00000001;
+    for (int i = 0; i < 8; i++) {
+      digitalWrite(latchPin, LOW);
+      shiftOut(dataPin, clockPin, MSBFIRST, 255);
+      shiftOut(dataPin, clockPin, MSBFIRST, 255);
+      shiftOut(dataPin, clockPin, MSBFIRST, n == 0 ? r : 0);
+      shiftOut(dataPin, clockPin, MSBFIRST, n == 1 ? r : 0);
+      digitalWrite(latchPin, HIGH);
+      r = r << 1;
+    }
+  }
+}
+
+void oneString() {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, 255);
+  shiftOut(dataPin, clockPin, MSBFIRST, 255);
+  shiftOut(dataPin, clockPin, MSBFIRST, B10000000);
+  shiftOut(dataPin, clockPin, MSBFIRST, 0);
+  digitalWrite(latchPin, HIGH);
+}
+
+void displayRandomPixel(short count) {
+  short x = random(0, 15);
+  short y = random(0, 15);
+  unsigned int snapshot[16][16] = {
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  };
+  snapshot[x][y] = 1;
+  unsigned int t = millis();
+  for (short i = 0; i < count; i++) {
+    while (millis() < t + 1000) {
+      render(snapshot);
+    }
+    delay(85);
+    t = millis();
+  }
+  delay(260);
+}
+
+void renderLine(int number) {
+  unsigned int snapshot[16][16] = {
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  };
+  snapshot[number][0] = 1;
+  snapshot[number][1] = 1;
+  snapshot[number][2] = 1;
+  snapshot[number][3] = 1;
+  snapshot[number][5] = 1;
+  snapshot[number][6] = 1;
+  snapshot[number][7] = 1;
+  snapshot[number][8] = 1;
+  snapshot[number][9] = 1;
+  snapshot[number][10] = 1;
+  snapshot[number][11] = 1;
+  snapshot[number][12] = 1;
+  snapshot[number][13] = 1;
+  snapshot[number][14] = 1;
+  snapshot[number][15] = 1;
+  render(snapshot);
+}
+
+void fillCup() {
+  unsigned int clearSnapshot[16][16] = {
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  };
+  unsigned int timer = millis();
+  for (int i = 15; i >= 0; i = i - 3) {
+    unsigned int snapshot[16][16] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    };
+    for (int x = 15; x >= i; x--) {
+      for (int n = 0; n < 16; n++) {
+        snapshot[x][n] = 1;
+      }
+      while (millis() < (timer + 200)) {
+        render(snapshot);
+      }
+      timer = millis();
+    }
+    while (millis() < (timer + 200)) {
+      render(clearSnapshot);
+    }
+    timer = millis();
+  }
+}
+
+void animation7() {
+  unsigned int snapshot[16][16] = {
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+  renderByTime(snapshot, 200);
+  int rowDelay = 150;
+  short m = 15;
+  for (short x = 0; x < 3; x++) {
+    for (short n = m; n > m - 3; n--) {
+      for (short i = 0; i < 16; i++) {
+        snapshot[n][i] = 0;
+      }
+      renderByTime(snapshot, rowDelay);
+    }
+    for (short i = 0; i < 16; i++) {
+      snapshot[x][i] = 0;
+    }
+    renderByTime(snapshot, rowDelay);
+    m = m - 3;
+  }
+  for (short n = 6; n > 3; n--) {
+    for (short i = 0; i < 16; i++) {
+      snapshot[n][i] = 0;
+    }
+    renderByTime(snapshot, rowDelay);
+  }
+  for (short i = 0; i < 16; i++) {
+    snapshot[3][i] = 0;
+    renderByTime(snapshot, 50);
+  }
+}
+
+void diagonal(unsigned long delay) {
+  unsigned int snapshot[16][16] = {
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  };
+  unsigned long timer = millis();
+  while (millis() < timer + delay) {
+    for (short i = 15; i >= 0; i--) {
+      for (short n = 0; n < 15; n++) {
+        for (short val = 0; val < 16; val++) {
+          snapshot[n][val] = snapshot[n + 1][val];
+        }
+      }
+      for (short val = 0; val < 16; val++) {
+        snapshot[15][val] = 0;
+      }
+      snapshot[15][i] = 1;
+      renderByTime(snapshot, 50);
+    }
+  }
+}
+
+void animation7Revert(unsigned int d) {
+  unsigned int snapshot[16][16] = {
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  };
+  int rowDelay = 75;
+  short m = 7;
+  for (short i = 15; i >= 0; i--) {
+    snapshot[3][i] = 1;
+    renderByTime(snapshot, 25);
+  }
+
+  renderByTime(snapshot, d);
+
+  for (short n = 4; n < 7; n++) {
+    for (short i = 0; i < 16; i++) {
+      snapshot[n][i] = 1;
+    }
+    renderByTime(snapshot, rowDelay);
+  }
+
+  for (short x = 2; x >= 0; x--) {
+    for (short n = m; n < m + 3; n++) {
+      for (short i = 0; i < 16; i++) {
+        snapshot[n][i] = 1;
+      }
+      renderByTime(snapshot, rowDelay);
+    }
+    for (short i = 0; i < 16; i++) {
+      snapshot[x][i] = 1;
+    }
+    renderByTime(snapshot, rowDelay);
+    m = m + 3;
+  }
+}
+
+void slive() {
+  unsigned int snapshot[16][16] = {
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  };
+  renderByTime(snapshot, 1000);
+}
+
+void TFrame(){
+  unsigned int snapshot[16][16] = {
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+  };
+  for (int i = 15; i >= 4; i--) {
+    snapshot[i][7] = 1;
+    renderByTime(snapshot, 130);
+  }
+  for (int i = 8, n = 7; i < 15; i++) {
+    snapshot[4][i] = 1;
+    snapshot[4][n] = 1;
+    n--;
+    renderByTime(snapshot, 130);
+  }
+  renderByTime(snapshot, 500);
 }
 
 void loop() {
- // отсчитываем от 0 до 255  и отображаем значение на светодиоде
- int row = 1;
- for (int i = 1; i < 9; i++) {
-   // устанавливаем синхронизацию "защелки" на LOW
-   digitalWrite(latchPin, LOW);
-
-   shiftOut(dataPin, clockPin, MSBFIRST, row);
-   shiftOut(dataPin, clockPin, MSBFIRST, 0);
-
-   shiftOut(dataPin, clockPin, MSBFIRST, 255);
-   shiftOut(dataPin, clockPin, MSBFIRST, 255);
-   //"защелкиваем" регистр, тем самым устанавливая значения на выходах
-   digitalWrite(latchPin, HIGH);
-   row = row << i;
- }
+  TFrame();
+  return;
+  unsigned long timer = millis();
+  while (millis() < timer + 21000) {
+    displayRandomPixel(2);
+  }
+  timer = millis();
+  fillCup();
+  while (millis() < timer + 11000) {
+    fillFull();
+  }
+  animation7();
+  animation7Revert(0);
+  animation(7000);
+  diagonal(6000);
+  animation7Revert(5000);
 }
